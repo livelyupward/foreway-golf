a4c470480f49cbe1ba8f10f15a69d026d4545371
Dave Grammas <dave@Daves-MacBook-Pro-2.local>
2023-04-03 18:27:02 -0500

    Thinking through this I think prod errors are due to nginx doing the rewrite. Vite shouldn't do the rewrite in the case of production. Set a env ternary to handle dev vs. prod.

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,24 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  base: './',
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:4000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [vue()],
+    base: './',
+    server: {
+      proxy:
+        process.env.VITE_APP_ENV === 'dev'
+          ? {
+              '/api': {
+                target: 'http://localhost:4000',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, ''),
+              },
+            }
+          : undefined,
     },
-  },
-});
+  });
+};
