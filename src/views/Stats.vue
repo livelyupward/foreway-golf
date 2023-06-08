a09a396da77dd018fa3223b354ff6e4a91f14928
Dave Grammas <dave@livelyupward.dev>
2023-06-07 22:29:58 -0500

    Started adding ideas to the stats view.

--- src/views/Stats.vue
+++ src/views/Stats.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="stats-page">
     <h1 class="stats-page_title page-title">My Stats</h1>
+    <h2>Handicap: 18</h2>
+    <h2>Scoring average: 90</h2>
+    <h2>Putts/hole: 2.8</h2>
+    <h2>GIR %: 31</h2>
+    <h2>Fairway %: 42</h2>
   </div>
 </template>
 
